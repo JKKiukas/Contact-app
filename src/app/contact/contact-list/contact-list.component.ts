37cddf18d203aa8fd9070cd8f080efaad371c56b
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-05-20 10:20:36 +0300

    Added on click alert.

--- src/app/contact/contact-list/contact-list.component.ts
+++ src/app/contact/contact-list/contact-list.component.ts
@@ -13,11 +13,16 @@ export class ContactListComponent implements OnInit {
     this.contacts = [];
   }
 
+  onContactSelected(contact: Contact): void {
+    console.log(contact);
+    alert(contact.firstName + ' ' + contact.lastName + ' ' + contact.phoneNumber);
+  }
+
+
   ngOnInit() {
     this.contacts.push(new Contact('Pekka', 'Korhonen', '040123456'));
     this.contacts.push(new Contact('Jouko', 'Virtanen', '040654321'));
     this.contacts.push(new Contact('Matti', 'Koivunen', '040162534'));
     console.log(this.contacts);
   }
-
 }
